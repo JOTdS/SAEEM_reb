10683bde1399d932e444f42f2ecffb7e1294619e
JOTdS <j.otavio.ts@gmail.com>
2019-09-25 14:26:00 -0300

    Criando os models e migrations. Fazendo as migrations de Pessoa, Aluno, Professor, Gestor, Funcionário e Escolas

---
+++ database/migrations/2019_09_25_114750_create_pessoas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePessoasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pessoas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome', 100);
+            $table->string('cpf', 11)->unique();
+            $table->string('telefone')->nullable();
+            $table->text('endereco')->nullable();
+            $table->text('descricao')->nullable();
+            $table->string('password');
+            $table->boolean('is_administrador')->default(false);
+            $table->boolean('is_professor')->default(false);
+            $table->boolean('is_aluno')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pessoas');
+    }
+}
---
+++ database/migrations/2019_09_25_131557_create_escolas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEscolasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('escolas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->text('endereco');
+            $table->integer('id_gestor');
+            $table->string('telefone');
+            $table->string('modalidade');
+            $table->string('inep');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('escolas');
+    }
+}
