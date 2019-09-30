e4b98e78f6a45ef7ad3a73064f31e1923e95cc31
Renato-P-Silva <renato.ps08@hotmail.com>
2019-09-29 22:11:43 -0300

    apenas alguns testes

---
+++ database/seeds/AvaliacaoSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class AvaliacaoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      // $faker = Faker::create();
+    	// foreach (range(1,4) as $index) {
+	    //     DB::table('avaliacaos')->insert([
+      //         'data' => $faker->date,
+      //         'nota' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 10),
+      //         'descricao' => $faker->text($maxNbChars = 240),
+	    //     ]);
+      // }
+	}
+}
