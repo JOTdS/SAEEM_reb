e4befcb46d3514a841c3410ee2959390ccfc4fe6
JOTdS <j.otavio.ts@gmail.com>
2019-09-29 21:35:37 -0300

    Movendo atributo 'sexo' da classe 'Aluno' para a classe 'Pessoa' e refazendo as migrations e seeders

--- database/seeds/AlunoSeeder.php
+++ database/seeds/AlunoSeeder.php
@@ -15,22 +15,22 @@ class AlunoSeeder extends Seeder
         for($g = 1; $g <= 483; $g++){
             $nome =$faker->name;
 
+            $sexo = ['M', 'F'];
             \App\Pessoa::create(['nome' => $nome,
                 'cpf' => $faker->numberBetween(10000000000, 99999999999),
                 'telefone' => $faker->phoneNumber,
                 'endereco' => $faker->address,
                 'descricao' => $faker->text(200),
                 'password' => $faker->text(32),
-                'is_administrador' => true]);
+                'sexo' => $sexo[$faker->numberBetween(0,1)],
+                'is_aluno' => true]);
 
             $pes = DB::select('select * from pessoas where nome = ?', [$nome]);
-            $sexo = ['M', 'F'];
 
 
             \App\Aluno::create(['id_pessoa' => $pes[0]->id,
                 'id_turma' => null,
                 'nascimento' => $faker->date('d-m-Y'),
-                'sexo' => $sexo[$faker->numberBetween(0,1)],
                 'filiacao' => $faker->name]);
         }
     }
